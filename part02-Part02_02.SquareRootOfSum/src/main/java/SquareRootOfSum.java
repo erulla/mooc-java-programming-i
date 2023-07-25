6302627c509f715ad6577a56501d1e19efcf8418
Chik Fong Eugene FOO <102475213+erulla@users.noreply.github.com>
2023-07-26 01:06:57 +0800

    Completed till Part02_06

--- part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
+++ part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
@@ -5,6 +5,8 @@ public class SquareRootOfSum {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        Integer firstNumber = Integer.valueOf(scanner.nextLine());
+        Integer secondNumber = Integer.valueOf(scanner.nextLine());
+        System.out.println(Math.sqrt(firstNumber+secondNumber));
     }
 }
--- part02-Part02_05.CarryOn/src/main/java/CarryOn.java
+++ part02-Part02_05.CarryOn/src/main/java/CarryOn.java
@@ -5,6 +5,12 @@ public class CarryOn {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        while (true) {           
+            System.out.println("Shall we carry on?"); 
+            String answer = scanner.nextLine();
+            if (answer.equals("no")) { //to not use ==
+                break;
+            }
+        }
     }
 }
