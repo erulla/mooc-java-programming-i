9b765b19e93fde7ea4c1fea1f0d70dafdb4be54f
Chik Fong Eugene FOO <102475213+erulla@users.noreply.github.com>
2023-07-31 16:44:21 +0800

    added part 03 files

--- part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -5,6 +5,19 @@ public class AverageOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int countOfNumbers = 0;
+        int sumOfNumbers = 0;
+        while (true) {
+            System.out.println("Give a number:");
+            int newNumber = Integer.valueOf(scanner.nextLine());
+            if (newNumber!=0) {
+                countOfNumbers+=1;
+                sumOfNumbers+=newNumber;
+                continue;
+            } else {
+                break;
+            }
+        }
+        System.out.println("Average of the numbers: " + ((float)sumOfNumbers/countOfNumbers));
     }
 }
